50130ccfb3f64b9086b8b0aaec3437bd8787b096
shawn25n92 <shawn25n92@gmail.com>
2017-11-03 18:03:29 +0800

    HashTableBuilder
    Writing an algorithm that can build hash tables for MD5, SHA-1, SHA-256, SHA-512

---
+++ JARVIS5/JARVISRandomAlgorithms.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JARVIS5
+{
+    public static class JARVISRandomAlgorithms
+    {
+        public static StatusObject BuildUUIDTable()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+                JARVISDataSource newDataSource = new JARVISDataSource("sql2008kl", "shawn_db", "sa", "password");
+                Guid.NewGuid();
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+        public static StatusObject BuildHashTable()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+                // Drop table RainbowTable
+                // Create table RainbowTable
+                // Dictionary words
+                // Take l33t5p34k into consideration
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+    }
+}
