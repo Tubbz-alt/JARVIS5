92b23751b1284954a06b6042fe592662ef3a5ac1
shawn25n92 <shawn25n92@gmail.com>
2017-11-08 00:12:25 +0800

    Updated Dictionary for Batch Escape characters

--- JARVIS5/JARVISUniversalDefinitions.cs
+++ JARVIS5/JARVISUniversalDefinitions.cs
@@ -8,9 +8,63 @@ namespace JARVIS5
 {
     public static class JARVISUniversalDefinitions
     {
+        public static Dictionary<char,string> GetBatchFileEscapeCharacters()
+        {
+            Dictionary<char, string> BatchFileEscapeCharacters = new Dictionary<char, string>();
+            try
+            {
+                BatchFileEscapeCharacters.Add('%', "%%");
+                BatchFileEscapeCharacters.Add('^', "^^");
+                BatchFileEscapeCharacters.Add('&', "^&");
+                BatchFileEscapeCharacters.Add('>', "^>");
+                BatchFileEscapeCharacters.Add('<', "^<");
+                BatchFileEscapeCharacters.Add('|', "^|");
+                BatchFileEscapeCharacters.Add('\'', "^'");
+                BatchFileEscapeCharacters.Add('`', "^`");
+                BatchFileEscapeCharacters.Add(',', "^,");
+                BatchFileEscapeCharacters.Add(';', "^;");
+                BatchFileEscapeCharacters.Add('=', "^=");
+                BatchFileEscapeCharacters.Add('(', "^(");
+                BatchFileEscapeCharacters.Add(')', "^)");
+                BatchFileEscapeCharacters.Add('!', "^^!");
+                BatchFileEscapeCharacters.Add('\"', "\"\"");
+                BatchFileEscapeCharacters.Add('\\', "\\\\");
+                BatchFileEscapeCharacters.Add('[', "\\[");
+                BatchFileEscapeCharacters.Add(']', "\\]");
+                BatchFileEscapeCharacters.Add('.', "\\.");
+                BatchFileEscapeCharacters.Add('*', "\\*");
+                BatchFileEscapeCharacters.Add('?', "\\?");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                BatchFileEscapeCharacters = null;
+            }
+            return BatchFileEscapeCharacters;
+        }
         public static Dictionary<char, string> BatchFileEscapeCharacters = new Dictionary<char, string>()
         {
-            
+                {'%', "%%"},
+                {'^', "^^"},
+                {'&', "^&"},
+                {'>', "^>"},
+                {'<', "^<"},
+                {'|', "^|"},
+                {'\'', "^'"},
+                {'`', "^`"},
+                {',', "^,"},
+                {';', "^;"},
+                {'=', "^="},
+                {'(', "^("},
+                {')', "^)"},
+                {'!', "^^!"},
+                {'\"', "\"\""},
+                {'\\', "\\\\"},
+                {'[', "\\["},
+                {']', "\\]"},
+                {'.', "\\."},
+                {'*', "\\*"},
+                {'?', "\\?" }
         };
     }
 }
